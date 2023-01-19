3a392901a744bcf0ef9b0e613820f385f6ad8e9c
altairbaku <manav.prasad.27@gmail.com>
2023-01-19 21:25:23 +0100

    solved day 24

--- day24/sol.py
+++ day24/sol.py
@@ -1,8 +1,31 @@
+from itertools import combinations
+
+def optimal_group(packages,group_size):
+    package_sum = sum(packages)/group_size
+    smallest_group_size = 2
+    combos = set()
+    while True:
+        combos = [seq for seq in combinations(packages, smallest_group_size)
+          if sum(seq) == package_sum]
+        if combos:
+            break
+        smallest_group_size += 1
+    best = 100000000000
+    for combo in combos:
+        result = 1
+        for x in combo:
+            result = result * x
+        if result < best:
+            best= result
+    return best
+
 with open('input.txt') as f:
     package_list = f.readlines()
 
-packages = []
+packages = set()
 for package in package_list:
-    packages.append(int(package))
+    packages.add(int(package))
 
-print(len(packages))
+
+print(optimal_group(packages,3))
+print(optimal_group(packages,4))
